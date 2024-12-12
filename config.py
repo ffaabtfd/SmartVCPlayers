120271f17d1cf7a00cf3840512b62519c87b2e70
Abir Arafat Chawdhury <abirthedigitalguru@gmail.com>
2024-12-12 23:14:38 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 # Copyright (C) @subinps
 # Update By (C) @theSmartBisnu
-# ReUpdate By (C) @abirxdhackz
-# Channel : https://t.me/abir_xd_bio
-
+# Channel : https://t.me/itsSmartDev
+#abirxdhackz
 
 from utils import LOGGER
 try:
@@ -36,7 +35,7 @@ class Config:
     LOG_GROUP=os.environ.get("LOG_GROUP", "")
 
     #Cookies For Youtube Downloder
-    YT_COOKIES_PATH = "/root/SmartVCPlayer/ytcookies/cookies.txt"
+    YT_COOKIES_PATH = os.path.join(os.getcwd(), "ytcookies", "cookies.txt")
 
     #Stream 
     STREAM_URL=os.environ.get("STARTUP_STREAM", "https://d2q8p4pe5spbak.cloudfront.net/bpk-tv/9XM/9XM.isml/9XM-audio_208482_und=208000-video=2137600.m3u8")
